f347c04c5a054e634d05e8266dc6a002e9343b06
Jason Liang <lainbo@foxmail.com>
2023-10-10 23:07:54 +0800

    chore: 🔧 更新版本

--- src/assets/template.js
+++ src/assets/template.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name 全局自定义字体
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  修改为自定义字体
 // @license      MIT
 // @author       Lainbo
