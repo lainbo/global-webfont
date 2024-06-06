3ada800b17b29ab617a71ff8130dc0ab7c7c39da
liangjiacheng <lainbo@foxmail.com>
2024-06-06 16:54:39 +0800

    Update font-family for notion code blocks

--- src/assets/template.js
+++ src/assets/template.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name 全局自定义字体
 // @namespace    http://tampermonkey.net/
-// @version      1.13
+// @version      1.14
 // @description  修改为自定义字体
 // @license      MIT
 // @author       Lainbo
